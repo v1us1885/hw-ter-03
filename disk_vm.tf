5d53c3c9f38ca04156beae4d1f6b99332a343a7d
v1us1885 <v1rus1885@mail.ru>
2024-07-07 19:27:30 +0300

    Create disk_vm.tf

---
+++ disk_vm.tf
@@ -0,0 +1,41 @@
+resource "yandex_compute_disk" "disk" {
+  count = var.disk_count
+
+  name = "disk-${count.index + 1}"
+  type = "network-hdd"
+  size = 1
+  zone = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "storage" {
+  name        = "storage"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.vm_web_image_family
+    }
+  }
+
+  dynamic "secondary_disk" {
+    for_each = "${yandex_compute_disk.disk.*.id}"
+    content {
+      disk_id     = yandex_compute_disk.disk["${secondary_disk.key}"].id
+    }
+  }
+
+  metadata = local.metadata
+  scheduling_policy { preemptible = true }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+     security_group_ids = toset([yandex_vpc_security_group.example.id])
+  }
+}
